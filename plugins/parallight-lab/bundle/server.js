9b485dc39b894a759ff0769ec32f8257e292bbb1
Marvin Gao <mrvgao@users.noreply.github.com>
2026-06-17 21:27:19 -0700

    publish 0.1.15-phase1

--- plugins/parallight-lab/bundle/server.js
+++ plugins/parallight-lab/bundle/server.js
@@ -30954,7 +30954,7 @@ var StdioServerTransport = class {
 };
 
 // ../shared/src/index.ts
-var PARALLIGHT_VERSION = "0.1.14-phase1";
+var PARALLIGHT_VERSION = "0.1.15-phase1";
 
 // src/config.ts
 import { homedir } from "node:os";
